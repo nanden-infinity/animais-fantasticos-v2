43131efad5ec135aba21f32e4c1ada6cae539516
Issa Tchamo <sanha@mac.home>
2026-03-24 13:46:37 +0000

    export add last file

--- js/modules/accordion.js
+++ js/modules/accordion.js
@@ -1,8 +1,8 @@
 export default function initAccordion() {
   const accordionList = document.querySelectorAll(
-    '[data-anime="accordion"] dt'
+    '[data-anime="accordion"] dt',
   );
-  const activeClass = 'ativo';
+  const activeClass = "ativo";
 
   function activeAccordion() {
     this.classList.toggle(activeClass);
@@ -12,8 +12,8 @@ export default function initAccordion() {
     accordionList[0].classList.add(activeClass);
     accordionList[0].nextElementSibling.classList.add(activeClass);
 
-    accordionList.forEach(item => {
-      item.addEventListener('click', activeAccordion);
+    accordionList.forEach((item) => {
+      item.addEventListener("click", activeAccordion);
     });
   }
 }
